33bdc33437c8a8f2747595988a0d0e02fca3d843
Tao Hu <taohu2222@gmail.com>
2023-09-26 15:49:27 +0800

    Setup amplify and the settings.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css"
+import TestPage from './pages/testPage/TestPage';
+import { Amplify } from 'aws-amplify';
+import awsExports from "./aws-exports"
+import { AWSIoTProvider } from '@aws-amplify/pubsub';
 
-function App() {
+Amplify.configure(awsExports)
+Amplify.addPluggable(
+  new AWSIoTProvider({
+    aws_pubsub_region: awsExports.aws_pubsub_region,
+    aws_pubsub_endpoint: awsExports.aws_pubsub_endpoint
+  })
+)
+
+function App({signOut, user}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign Out</Button>
+      <TestPage/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
