8ccbcba3989bea553cc53b6d44d5d99b9d936cd8
Tao Hu <taohu2222@gmail.com>
2023-09-29 13:43:06 +0800

    remove uncessary comment

--- aws/lambda/iotDeviceGameDataLambda/index.js
+++ aws/lambda/iotDeviceGameDataLambda/index.js
@@ -2,17 +2,6 @@ const AWS = require('aws-sdk');
 const {v4: uuidv4} = require("uuid");
     
 const dynamo = new AWS.DynamoDB.DocumentClient();
-
-/**
- * Demonstrates a simple HTTP endpoint using API Gateway. You have full
- * access to the request and response payload, including headers and
- * status code.
- *
- * To scan a DynamoDB table, make a GET request with the TableName as a
- * query string parameter. To put, update, or delete an item, make a POST,
- * PUT, or DELETE request respectively, passing in the payload to the
- * DynamoDB API as a JSON body.
- */
  
  const tableName = "GameData";
  
